844987106da869601073544dbcdef72ea0a7a537
LuoBingjun <luobingjun@gmail.com>
2019-04-15 17:46:01 +0800

    Algorithm HW6 finished

--- Algorithms/HW6/P3选做/report.md
+++ Algorithms/HW6/P3选做/report.md
@@ -1,8 +1,46 @@
 # 实验报告
-设计一个$O(n\lg n)$的算法，求一个n个数的序列的最长单调递增子序列(LIS).
+
+具体实现一个将图像进行Seam carving的程序，其基本功能是可以将$m\times n$的图象压缩为$m/2\times n/2​$，并且有方便的输入输出功能.
+
 
 ## 实验环境
 
+CPU：Intel Core i5-7200U
+
+内存：8GB
+
+操作系统：Windows 10 64位教育版
+
+编程语言：Python 3.6
+
+运行方式：直接运行seam_carving.exe文件即可
+
 ## 算法分析
 
+### 计算破坏度
+
+参考网络资料（[地址](https://zhuanlan.zhihu.com/p/38974520)），选择了能量函数
+
+$$d_1(I)=|\frac{\partial}{\partial x}I|+|\frac{\partial}{\partial y}I|$$
+
+表示图像上每一点的破坏度，计算的时间复杂度为$O(m\times n)​$.
+
+### 查找接缝
+
+采用动态规划算法，详见P3(b)，时间复杂度为$O(m\times n)$.
+
+### 删除接缝
+
+反复调用计算破坏度和查找接缝函数，并逐次删除接缝，直到图片大小达到预期为止，时间复杂度为$O(mn(m+n))​$.
+
 ## 结果分析
+
+我们选择分辨率分别为480x300和600x345的几张不同风格的图片，运行了Seam carving算法，结果如下：
+
+![](C:\Projects\Homework\Algorithms\HW6\P3选做\Figure_3.jpg)
+
+![](C:\Projects\Homework\Algorithms\HW6\P3选做\Figure_4.jpg)
+
+![](C:\Projects\Homework\Algorithms\HW6\P3选做\Figure_1.jpg)
+
+结果表明，Seam carving对于大部分图片都能够比较好地保留图片中的关键信息，但对复杂形状（如图2和图3中的云）的处理容易变形，对物体边缘的处理也不够平滑.
--- Algorithms/HW6/P3选做/seam_carving.py
+++ Algorithms/HW6/P3选做/seam_carving.py
@@ -1,9 +1,11 @@
 import copy
-import tkinter
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 from scipy.ndimage.filters import convolve
+from tkinter import *
+import tkinter.filedialog
+import threading
 
 # 计算破坏度
 def calc_d(img):
@@ -73,11 +75,10 @@ def search_seam(img):
         seam.append(copy.copy(min_index))
     return seam
 
+# 删除接缝
 def carve_column(img):
     m, n, _ = img.shape
 
-    # 创建一个(r,c)矩阵，填充值为True
-    # 后面会从值为False的图像中移除所有像素
     mask = np.ones((m, n), dtype=np.bool)
 
     seam = search_seam(img)
@@ -85,37 +86,69 @@ def carve_column(img):
     for i in range(m):
         mask[m-i-1][seam[i]]=False
 
-    # 因为图像有3个通道，我们将蒙版转换为3D
     mask = np.stack([mask] * 3, axis=2)
 
-    # 删除蒙版中所有标记为False的像素，
-    # 将其大小重新调整为新图像的维度
     img = img[mask].reshape((m, n - 1, 3))
 
     return img
 
-def crop_column(img, scale):
+# 缩小列数
+def crop_column(img, scale, t=False):
     m, n, _ = img.shape
 
     img_new=copy.copy(img)
 
-    for i in range(int(scale * n)):
-        print(i)
-        img_new=carve_column(img_new)
-        print(img_new.shape[0],img_new.shape[1])
+    if t:
+        for i in range(int(scale * n)):
+            img_new=carve_column(img_new)
+            text1.set('当前图片大小：%dx%d'%(img_new.shape[1],img_new.shape[0]))
+    else:
+        for i in range(int(scale * n)):
+            img_new=carve_column(img_new)
+            text1.set('当前图片大小：%dx%d'%(img_new.shape[0],img_new.shape[1]))
 
     return img_new
 
-
+# 缩小行数
 def crop_row(img, scale):
     img = np.rot90(img, 1, (0, 1))
-    img = crop_column(img, scale)
+    img = crop_column(img, scale, t=True)
     img = np.rot90(img, 3, (0, 1))
     return img
 
-img_0=mpimg.imread('sample.jpg')
-img_new=crop_column(img_0,0.5)
-img_new=crop_row(img_new,0.5)
-plt.imshow(img_new)
-plt.axis('off')
-plt.show()
+def op_img(path):
+    img_0=mpimg.imread(path)
+    text2.set('当前图片大小：%dx%d'%(img_0.shape[0],img_0.shape[1]))
+    text2.set('预计图片大小：%dx%d'%(img_0.shape[0]-img_0.shape[0]//2,img_0.shape[1]-img_0.shape[1]//2))
+    img_new=crop_column(img_0,0.5)
+    img_new=crop_row(img_new,0.5)
+    plt.subplot(1, 2, 1)
+    plt.title('origin')
+    plt.imshow(img_0)
+    plt.subplot(1, 2, 2)
+    plt.title('new')
+    plt.imshow(img_new)
+    plt.show(block=True)
+
+def select():
+    path = tkinter.filedialog.askopenfilename()
+    if path != '':
+        t=threading.Thread(target=op_img,args=(path,))
+        t.setDaemon(True)
+        t.start()
+
+if __name__ == '__main__':
+    window = Tk()
+    window.title('Seam Carving Example')
+    window.geometry("240x80")
+    text1 = StringVar()
+    text2 = StringVar()
+    text1.set('当前图片大小：')
+    text2.set('预计图片大小：')
+    label1 = Label(window,textvariable=text1)
+    label1.pack()
+    label2 = Label(window,textvariable=text2)
+    label2.pack()
+    btn0 = Button(window,text="选择图片文件(.jpg)",command=select)
+    btn0.pack()
+    window.mainloop()
