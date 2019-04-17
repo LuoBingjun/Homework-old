3fc3e441b5360331257ff95930fb9fc0ca7a72f8
LuoBingjun <luobingjun@gmail.com>
2019-04-17 11:51:18 +0800

    level2 finished

---
+++ buflab-handout/level2.s
@@ -0,0 +1,14 @@
+
+level2.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	68 82 8b 04 08       	push   $0x8048b82
+   5:	55                   	push   %ebp
+   6:	89 e5                	mov    %esp,%ebp
+   8:	8b 1d 0c e1 04 08    	mov    0x804e10c,%ebx
+   e:	89 1d 04 e1 04 08    	mov    %ebx,0x804e104
+  14:	c9                   	leave  
+  15:	c3                   	ret    
